fe37586247d48d6d5dfcd2cb06abe3ec3356f55c
torinunna <torinunna9588@gmail.com>
2023-06-11 15:40:30 +0900

    add components to ButtonBenefitVC

--- TossBenefitPractice/Benefit/ButtonBenefitViewController.swift
+++ TossBenefitPractice/Benefit/ButtonBenefitViewController.swift
@@ -9,21 +9,12 @@ import UIKit
 
 class ButtonBenefitViewController: UIViewController {
 
+    var benefit: Benefit = .today
+    var benefitDetails: BenefitDetails = .default
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.largeTitleDisplayMode = .never
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
