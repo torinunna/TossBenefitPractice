91d9d555c6540a4cc1ebf9deb37d9f0b359444ce
torinunna <torinunna9588@gmail.com>
2023-06-11 15:03:18 +0900

    create Benefit - ButtonBenefit.storyboard, Button BenefitVC

---
+++ TossBenefitPractice/Benefit/ButtonBenefitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ButtonBenefitViewController.swift
+//  TossBenefitPractice
+//
+//  Created by YUJIN KWON on 2023/06/11.
+//
+
+import UIKit
+
+class ButtonBenefitViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
