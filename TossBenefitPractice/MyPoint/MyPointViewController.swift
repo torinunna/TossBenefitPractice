f676c84596089e86b40b5937f89c9756b3b93c78
torinunna <torinunna9588@gmail.com>
2023-06-11 14:17:15 +0900

    create MyPoint - MyPoint.storyboard, MyPointVC

---
+++ TossBenefitPractice/MyPoint/MyPointViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyPointViewController.swift
+//  TossBenefitPractice
+//
+//  Created by YUJIN KWON on 2023/06/11.
+//
+
+import UIKit
+
+class MyPointViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
