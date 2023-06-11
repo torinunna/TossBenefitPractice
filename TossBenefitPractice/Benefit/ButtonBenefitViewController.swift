568d15f36403a9de4c9585eb199d4494b073aa5c
torinunna <torinunna9588@gmail.com>
2023-06-11 16:54:15 +0900

    add guideView to ButtonBenefitVC

--- TossBenefitPractice/Benefit/ButtonBenefitViewController.swift
+++ TossBenefitPractice/Benefit/ButtonBenefitViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ButtonBenefitViewController: UIViewController {
     @IBOutlet weak var ctaButton: UIButton!
+    @IBOutlet weak var vStackView: UIStackView!
     
     var benefit: Benefit = .today
     var benefitDetails: BenefitDetails = .default
@@ -16,10 +17,30 @@ class ButtonBenefitViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.largeTitleDisplayMode = .never
-        ctaButton.layer.masksToBounds = true
-        ctaButton.layer.cornerRadius = 5
         
+        setUpUI()
+        addguiedes()
         ctaButton.setTitle(benefit.ctaTitle, for: .normal)
     }
+    
+    private func setUpUI() {
+        ctaButton.layer.masksToBounds = true
+        ctaButton.layer.cornerRadius = 5
+    }
 
+    private func addguiedes() {
+        let guideView: [BenefitGuideView] = benefitDetails.guides.map { guide in
+            let guideView = BenefitGuideView(frame: .zero)
+            guideView.icon.image = UIImage(systemName: guide.iconName)
+            guideView.title.text = guide.guide
+            return guideView
+        }
+        
+        guideView.forEach { view in
+            self.vStackView.addArrangedSubview(view)
+            NSLayoutConstraint.activate([
+                view.heightAnchor.constraint(equalToConstant: 60)
+            ])
+        }
+    }
 }
