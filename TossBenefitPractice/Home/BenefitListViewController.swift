03cdbf5699f0d144ea9b69e19109cad6f5d64c35
torinunna <torinunna9588@gmail.com>
2023-06-12 15:05:24 +0900

    create BenefitListViewModel and refactor BenefitListVC

--- TossBenefitPractice/Home/BenefitListViewController.swift
+++ TossBenefitPractice/Home/BenefitListViewController.swift
@@ -20,17 +20,16 @@ class BenefitListViewController: UIViewController {
     }
 
     var datasource: UICollectionViewDiffableDataSource<Section, Item>!
-    
-    @Published var todaySectionItems: [AnyHashable] = []
-    @Published var otherSectionItems: [AnyHashable] = []
-    
     var subscriptions = Set<AnyCancellable>()
     
+    let viewModel: BenefitListViewModel = BenefitListViewModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         configureCollectionView()
         bind()
+        viewModel.fetchItems()
     }
     
     private func setupUI() {
@@ -46,38 +45,44 @@ class BenefitListViewController: UIViewController {
         
         var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
         snapshot.appendSections([.today, .other])
-        snapshot.appendItems(todaySectionItems, toSection: .today)
-        snapshot.appendItems(otherSectionItems, toSection: .other)
+        snapshot.appendItems([], toSection: .today)
+        snapshot.appendItems([], toSection: .other)
         datasource.apply(snapshot)
         
         collectionView.collectionViewLayout = layout()
         
         collectionView.delegate = self
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.todaySectionItems = TodaySectionItem(point: .default, today: .today).sectionItems
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-            self.otherSectionItems = Benefit.others
-        }
-    }
 
     private func bind() {
-        $todaySectionItems
+        viewModel.$todaySectionItems
             .receive(on: RunLoop.main)
             .sink { items in
                 self.applySnapshot(items: items, section: .today)
             }.store(in: &subscriptions)
         
-        $otherSectionItems
+        viewModel.$otherSectionItems
             .receive(on: RunLoop.main)
             .sink { items in
                 self.applySnapshot(items: items, section: .other)
+            }.store(in: &subscriptions)
+        
+        viewModel.benefitDidTapped
+            .receive(on: RunLoop.main)
+            .sink { benefit in
+                let sb = UIStoryboard(name: "ButtonBenefit", bundle: nil)
+                let vc = sb.instantiateViewController(withIdentifier: "ButtonBenefitViewController") as! ButtonBenefitViewController
+                vc.benefit = benefit
+                self.navigationController?.pushViewController(vc, animated: true)
+            }.store(in: &subscriptions)
+        
+        viewModel.pointDidTapped
+            .receive(on: RunLoop.main)
+            .sink { point in
+                let sb = UIStoryboard(name: "MyPoint", bundle: nil)
+                let vc = sb.instantiateViewController(withIdentifier: "MyPointViewController") as! MyPointViewController
+                vc.point = point
+                self.navigationController?.pushViewController(vc, animated: true)
             }.store(in: &subscriptions)
     }
     
@@ -126,23 +131,15 @@ class BenefitListViewController: UIViewController {
         
         return UICollectionViewCompositionalLayout(section: section)
     }
-    
 }
 
 extension BenefitListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let item = datasource.itemIdentifier(for: indexPath)
-        
         if let benefit = item as? Benefit {
-            let sb = UIStoryboard(name: "ButtonBenefit", bundle: nil)
-            let vc = sb.instantiateViewController(withIdentifier: "ButtonBenefitViewController") as! ButtonBenefitViewController
-            vc.benefit = benefit
-            navigationController?.pushViewController(vc, animated: true)
+            viewModel.benefitDidTapped.send(benefit)
         } else if let point = item as? MyPoint {
-            let sb = UIStoryboard(name: "MyPoint", bundle: nil)
-            let vc = sb.instantiateViewController(withIdentifier: "MyPointViewController") as! MyPointViewController
-            vc.point = point
-            navigationController?.pushViewController(vc, animated: true)
+            viewModel.pointDidTapped.send(point)
         } else {
             
         }
---
+++ TossBenefitPractice/Home/BenefitListViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  BenefitListViewModel.swift
+//  TossBenefitPractice
+//
+//  Created by YUJIN KWON on 2023/06/12.
+//
+
+import Foundation
+import Combine
+
+final class BenefitListViewModel {
+    @Published var todaySectionItems: [AnyHashable] = []
+    @Published var otherSectionItems: [AnyHashable] = []
+    
+    let benefitDidTapped = PassthroughSubject<Benefit, Never>()
+    let pointDidTapped = PassthroughSubject<MyPoint, Never>()
+    
+    func fetchItems() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.todaySectionItems = TodaySectionItem(point: .default, today: .today).sectionItems
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+            self.otherSectionItems = Benefit.others
+        }
+    }
+}
